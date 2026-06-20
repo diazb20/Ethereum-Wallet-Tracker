bfcf0f091cff2cc17b9d4d61ff4e2adcbdd6f86e
diazb20 <yousefrefahi4@gmail.com>
2026-06-20 10:52:00 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const { getBalance } = require("./wallet");
+const { isValidAddress } = require("./utils");
 
 async function main() {
   const walletAddress =
     "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045";
+
+  if (!isValidAddress(walletAddress)) {
+    console.error("Invalid Ethereum address");
+    return;
+  }
 
   try {
     const balance = await getBalance(walletAddress);
