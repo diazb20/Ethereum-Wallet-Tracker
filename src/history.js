689be1172569eeb8c44da5fc5b2ca35e448daf5a
diazb20 <yousefrefahi4@gmail.com>
2026-06-22 06:19:22 -0700

    Create history.js

---
+++ src/history.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+
+const historyFile = path.join(__dirname, "../wallet-history.json");
+
+function saveAddress(address) {
+  let addresses = [];
+
+  if (fs.existsSync(historyFile)) {
+    addresses = JSON.parse(
+      fs.readFileSync(historyFile, "utf8")
+    );
+  }
+
+  if (!addresses.includes(address)) {
+    addresses.push(address);
+
+    fs.writeFileSync(
+      historyFile,
+      JSON.stringify(addresses, null, 2)
+    );
+  }
+}
+
+module.exports = {
+  saveAddress,
+};
