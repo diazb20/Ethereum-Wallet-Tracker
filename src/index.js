cccfe2c5380b0742fa4e964adab0e89bfc1561c9
diazb20 <yousefrefahi4@gmail.com>
2026-06-23 10:57:32 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 const readline = require("readline");
 const { getBalance } = require("./wallet");
 const { isValidAddress } = require("./utils");
+const { saveAddress, getHistory } = require("./history");
+const { printHeader } = require("./logger");
+
+printHeader();
+
+const history = getHistory();
+
+if (history.length > 0) {
+  console.log("Recently Checked Wallets:");
+  history.slice(-5).forEach((address, index) => {
+    console.log(`${index + 1}. ${address}`);
+  });
+
+  console.log("");
+}
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -19,6 +34,8 @@ rl.question("Enter Ethereum wallet address: ", async (walletAddress) => {
 
     console.log("\nWallet:", walletAddress);
     console.log("Balance:", balance, "ETH");
+
+    saveAddress(walletAddress);
   } catch (error) {
     console.error(error.message);
   }
