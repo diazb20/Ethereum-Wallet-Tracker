cc734fdf51764e58e4f95f8ae7a6cce53b9fd1b3
diazb20 <yousefrefahi4@gmail.com>
2026-06-24 12:13:12 -0700

    Update wallet.js

--- src/wallet.js
+++ src/wallet.js
@@ -7,13 +7,22 @@ const provider = new ethers.JsonRpcProvider(
 async function getBalance(address) {
   try {
     const balance = await provider.getBalance(address);
-
     return ethers.formatEther(balance);
   } catch (error) {
     throw new Error("Failed to fetch wallet balance");
   }
 }
 
+async function getNetworkInfo() {
+  const network = await provider.getNetwork();
+
+  return {
+    name: network.name,
+    chainId: network.chainId.toString(),
+  };
+}
+
 module.exports = {
   getBalance,
+  getNetworkInfo,
 };
