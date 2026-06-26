6e5c59c78aa57f3256e256ed1804300e84fa5abf
diazb20 <yousefrefahi4@gmail.com>
2026-06-26 06:12:24 -0700

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -4,6 +4,15 @@ function isValidAddress(address) {
   return ethers.isAddress(address);
 }
 
+function formatError(error) {
+  if (!error) {
+    return "Unknown error.";
+  }
+
+  return error.message || "Unexpected error occurred.";
+}
+
 module.exports = {
   isValidAddress,
+  formatError,
 };
