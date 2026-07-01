566b71415c85b98132b4684a407afdb73717f47c
diazb20 <yousefrefahi4@gmail.com>
2026-07-01 03:52:51 -0700

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -4,17 +4,25 @@ function isValidAddress(address) {
   return ethers.isAddress(address);
 }
 
+function formatError(error) {
+  if (!error) {
+    return "Unknown error.";
+  }
+
+  return error.message || "Unexpected error occurred.";
+}
+
 function getCurrentTimestamp() {
   return new Date().toLocaleString();
+}
+
+function toChecksumAddress(address) {
+  return ethers.getAddress(address);
 }
 
 module.exports = {
   isValidAddress,
   formatError,
   getCurrentTimestamp,
-};
-
-module.exports = {
-  isValidAddress,
-  formatError,
+  toChecksumAddress,
 };
