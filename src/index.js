393d8da1dc522caebf2bd12a8168caf649772747
diazb20 <yousefrefahi4@gmail.com>
2026-07-26 12:08:23 -0700

    improve invalid address message

--- src/index.js
+++ src/index.js
@@ -31,8 +31,7 @@ const {
 
 rl.question("Enter Ethereum wallet address: ", async (walletAddress) => {
   if (!isValidAddress(walletAddress)) {
-    console.error("Invalid Ethereum address");
-    rl.close();
+console.error("Invalid wallet address.");    rl.close();
     return;
   }
 
