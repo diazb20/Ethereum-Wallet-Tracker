6571f3fc73649d27364cb5209e950c096fad485d
diazb20 <yousefrefahi4@gmail.com>
2026-07-29 12:20:21 -0700

    update welcome message

--- src/logger.js
+++ src/logger.js
@@ -6,8 +6,7 @@ function printHeader() {
   console.log(` Version: ${config.version}`);
   console.log("=================================");
   console.log(`Time: ${new Date().toLocaleString()}`);
-  console.log("Track Ethereum wallet balances from the command line.");
-  console.log("");
+console.log("Check your Ethereum wallet easily.");  console.log("");
 }
 
 module.exports = {
