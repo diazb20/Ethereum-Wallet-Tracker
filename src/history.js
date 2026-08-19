6c1cf66545d3a32b72a55b0cddf0e12a0eb2fb0b
diazb20 <yousefrefahi4@gmail.com>
2026-08-19 08:50:37 -0700

    Clean wallet history before saving

--- src/history.js
+++ src/history.js
@@ -24,8 +24,7 @@ if (address && addresses.indexOf(address) === -1) {
   addresses.push(address);
 
     if (addresses.length > maxHistory) {
-      addresses = addresses.slice(-maxHistory);
-    }
+addresses = addresses.slice(-maxHistory).filter(Boolean);    }
 
     fs.writeFileSync(
       historyFile,
