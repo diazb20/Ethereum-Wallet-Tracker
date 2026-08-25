84dc6a6f0ace1a66d2b6cce250dfef2ce4b68d27
diazb20 <yousefrefahi4@gmail.com>
2026-08-25 00:45:20 -0700

    Reduce wallet history limit

--- src/history.js
+++ src/history.js
@@ -6,7 +6,7 @@ const historyFile = path.resolve(
   "../wallet-history.json"
 );
 
-const maxHistory = 15;
+const maxHistory = 10;
 const historyEncoding = "utf8";
 
 function saveAddress(address) {
