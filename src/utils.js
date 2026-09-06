0e7c1c051eef2e015456b5e5a5a57e79d50623ab
diazb20 <yousefrefahi4@gmail.com>
2026-09-06 02:37:35 -0700

    Handle missing error messages safely

--- src/utils.js
+++ src/utils.js
@@ -9,7 +9,7 @@ function formatError(error) {
     return "Unknown error.";
   }
 
-return error.message || "Something went wrong.";
+return error?.message || "Something went wrong.";
 }
 
 function getCurrentTimestamp() {
