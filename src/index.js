619dcbff1b8b592e0ba75078774295ff59866566
diazb20 <yousefrefahi4@gmail.com>
2026-09-24 01:55:20 -0700

    Limit displayed wallet history

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ const history = getHistory()
 if (history.length > 0) {
   console.log("Recently Checked Wallets:");
 
-  history.slice(-5).forEach((address, index) => {
-    console.log(`${index + 1}. ${address}`);
+history.slice(-3).forEach((address, index) => {
+  console.log(`${index + 1}. ${address}`);
   });
 
 console.log();
